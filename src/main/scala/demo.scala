b467b92188697ef87104440d80910d666ebc8dd4
Baidurya Roy <xs107-bairoy@host601-s07.xs.xenon.team>
2021-04-07 14:13:01 +0530

    Minor Changes

--- src/main/scala/demo.scala
+++ src/main/scala/demo.scala
@@ -35,16 +35,28 @@ class institute{
   }
 
   def temp(): Unit = {
+    val conf = new SparkConf().setAppName("Spark").setMaster("local");
+    val sc = new SparkContext(conf)
+    val spark = SparkSession.builder().appName("Spark SQL").config(conf).getOrCreate()
+    import spark.implicits._
+
     val department1 = new Department("123456", "Computer Science")
     val department2 = new Department("789012", "Mechanical Engineering")
     val department3 = new Department("345678", "Theater and Drama")
     val department4 = new Department("901234", "Indoor Recreation")
+    val department = Seq(department1,department2,department3,department4)
+    val dfDepartment = department.toDF()
+    dfDepartment.show()
 
     val employee1 = new Employee("michael", "armbrust", "no-reply@berkeley.edu", 100000)
     val employee2 = new Employee("xiangrui", "meng", "no-reply@stanford.edu", 120000)
     val employee3 = new Employee("matei", null, "no-reply@waterloo.edu", 140000)
     val employee4 = new Employee(null, "wendell", "no-reply@princeton.edu", 160000)
     val employee5 = new Employee("michael", "jackson", "no-reply@neverla.nd", 80000)
+    val employee = Seq(employee1,employee2,employee3,employee4,employee5)
+    val dfEmployee = employee.toDF()
+    dfEmployee.show()
+
 
     val departmentWithEmployees1 = new DepartmentWithEmployees(department1, Seq(employee1, employee2))
     val departmentWithEmployees2 = new DepartmentWithEmployees(department2, Seq(employee3, employee4))
@@ -52,19 +64,20 @@ class institute{
     val departmentWithEmployees4 = new DepartmentWithEmployees(department4, Seq(employee2, employee3))
 
 
-    /*val departmentsWithEmployeesSeq1 = Seq(departmentWithEmployees1, departmentWithEmployees2)
+    val departmentsWithEmployeesSeq1 = Seq(departmentWithEmployees1, departmentWithEmployees2)
     val df1 = departmentsWithEmployeesSeq1.toDF()
-    display(df1)
+    df1.show()
 
     val departmentsWithEmployeesSeq2 = Seq(departmentWithEmployees3, departmentWithEmployees4)
     val df2 = departmentsWithEmployeesSeq2.toDF()
-    display(df2)*/
+    df2.show()
   }
 }
 
 
-object demo {
+object demo extends institute {
   def main(args: Array[String]): Unit = {
+    /*
     val conf = new SparkConf().setAppName("Spark").setMaster("local");
     val sc = new SparkContext(conf)
 
@@ -85,6 +98,11 @@ object demo {
     println("\n\n\n\n\n")
     val df1 = count1.toDF("Name","Name-Count")
     df1.show()
+    df1.printSchema()
+    df1.select("Name").show()
+    //df1.select($"Name", $"age" + 1).show()
+    //df1.filter($"age" > 21).show()
+    //df1.groupBy("age").count().show()
 
 
     val file = sc.textFile("/home/xs107-bairoy/xenonstack/l2/module4/spark/files/test.txt")
@@ -100,5 +118,12 @@ object demo {
     println("\n\n\n\n\n")
     val df2 = count2.toDF("Word","Word-Count")
     df2.show()
+
+    df2.createOrReplaceTempView("Word")
+    val sqlDF2 = spark.sql("SELECT * FROM Word where `Word-Count`>1")
+    sqlDF2.show()
+*/
+    val institute1 = new institute
+    institute1.temp()
   }
 }
