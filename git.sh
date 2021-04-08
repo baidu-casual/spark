c5a081967a4f1bea1347bb0219a1660d762b116a
Baidurya Roy <xs107-bairoy@host601-s07.xs.xenon.team>
2021-04-08 18:34:30 +0530

    Minor Changes!

--- git.sh
+++ git.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 
-if [[ "$*" == "pull" ]]; then
-    git pull
-else    
-    if [[ "$*" == "" ]]; then
-        mess="Minor Changes!"
-    else
-        mess="$*"
-    fi
-    git add .
-    echo "Message: $mess"
-    git commit -m "$mess"
-    git push -u origin master
+if [[ "$*" == "" ]]; then
+    mess="Minor Changes!"
+else
+    mess="$*"
 fi
+git pull
+git add .
+echo "Message: $mess"
+git commit -m "$mess"
+git push -u origin master
